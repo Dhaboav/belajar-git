4f3bbc9d11012f278e1a43cf2185379031f2b742
dhaboav <dhaboav@gmail.com>
2023-05-24 02:08:25 +0700

    Menambahkan rebase.py

---
+++ rebase.py
@@ -0,0 +1,4 @@
+"""
+Rebase berguna untuk mempermudah workflow terutama untuk melihat branch
+Karena jika terlalu banyak maka akan kesulitan dalam memahami perubahan terutama jika bekerja pada team yang besar.
+"""
