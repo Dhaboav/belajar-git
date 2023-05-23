d837aa1a300668bb73a6b0ba0e3551b4ee6bf334
dhaboav <dhaboav@gmail.com>
2023-05-24 01:47:56 +0700

    Menambahkan file percobaan.py

---
+++ percobaan.py
@@ -0,0 +1,10 @@
+message = """
+Mencoba fitur .gitignore pada git maupun github
+Untuk mempermudah maka dapat mencari pada browser gitignore untuk mengetahui file-file apa saja yang perlu diabaikan
+Sekarang mencoba membuat .gitignore secara manual
+
+Untuk .gitignore bisa mengabaikan file maupun folder yang ingin diabaikan.
+Kenapa karena ada beberapa file maupun folder yang jika di commit dan digunakan orang lain malah akan merepotkan.
+"""
+
+print(message)
